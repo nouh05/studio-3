fc33cd4e716141cf51c6ca6f5a059c4a0d6489d3
nouh05 <60891514+nouh05@users.noreply.github.com>
2022-09-29 21:57:17 -0400

    her

--- app.js
+++ app.js
@@ -26,15 +26,23 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
-
+  const filteredArray = fruitsArray.filter((item) => {
+    return item.nutritions.sugar > 15;
+  })
+  console.log(filteredArray);
+  const keyArray = filteredArray.map((item) => {
+    return item.name;
+  })
   // TODO: Create a new HTML element to display your data
-
+  list = keyArray.toString();
+  const element = document.createElement('p');
+  element.innerHTML = list;
   // TODO: Append your new element to the page
-
+  gallery.appendChild(element);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
